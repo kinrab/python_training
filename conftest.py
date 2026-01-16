6f74f826664f75b90cc572d834a9c16d7a1df991
kinrab <53529271+kinrab@users.noreply.github.com>
2026-01-16 17:09:05 +0300

    NEW RESTORE
    NEW RESTORE

--- conftest.py
+++ conftest.py
@@ -1,10 +1,21 @@
 
 import pytest
+import time
+
 from FIXTURE.application import Application
 
 
 @pytest.fixture (scope = "session")
 def app(request):
+
     fixture = Application()
-    request.addfinalizer(fixture.Destroy)
+    fixture.session.Login_process(username = "admin", password = "secret")
+    def fin():
+         time.sleep(3)
+         fixture.session.Logout_process()
+         time.sleep(3)
+         fixture.Destroy()
+
+    request.addfinalizer(fin)
     return fixture
+
