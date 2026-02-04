f3e4eff746e05ecb079d84fd9d1b6506031c95b4
kinrab <53529271+kinrab@users.noreply.github.com>
2026-02-04 15:08:54 +0300

    Add

--- jenkins_run.bat
+++ jenkins_run.bat
@@ -1,9 +1,4 @@
-
 echo "Start..."
-
-git branch: 'main', url: 'https://github.com/kinrab/python_training.git'
-
-python -m venv venv
 
 xcopy  D:\REPO_PYTHON\Python_Training\.venv C:\xxl\venv /E /I /H /Y
                 
