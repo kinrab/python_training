dbbae08ac4261ca5cbbfc0e27cb3718855011fdc
kinrab <53529271+kinrab@users.noreply.github.com>
2026-02-04 15:31:00 +0300

    Update jenkins_run.bat

--- jenkins_run.bat
+++ jenkins_run.bat
@@ -1,5 +1,4 @@
 echo "Start..."
-
-xcopy  D:\REPO_PYTHON\Python_Training\.venv C:\xxl\venv /E /I /H /Y
-                
+python -m venv venv
+xcopy  D:\REPO_PYTHON\Python_Training\.venv C:\xxl\venv /E /I /H /Y                
 echo "End of work."
